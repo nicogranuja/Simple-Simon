a17ed5e470f11e4ec493bb520231988e3af01049
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-08-19 12:28:25 -0500

    On the html file: 	I changed the layout instead of two rows I have one row and forur divs inside of it. On the css file: 	The container size was changed along with the property float left to organize the squares. On the Js: 	Kind of a clean start getting rid of intervals, but now the game lights up the buttons fine wiht animation.

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,7 +1,3 @@
-//button  clicked start with 1 random light display round number change text of the html
-//save the values randomized in order, make sure the user inputs them in order
-//make something that stops the game when user misses?
-
 $(document).ready(function() {
 	"use strict";
 	//variables for the game
@@ -16,9 +12,10 @@ $(document).ready(function() {
 	var $colors = $('.color');
 	var $divColors = $('.divColors');
 	//*jquery variables
-	var maxLevel = 50;//level at which the sequence is pre-generated
+	var maxLevel = 50; //level at which the sequence is pre-generated
 	var sequenceOfColors = []; //array that will hold the things in order
-	var count = 1; //counts the number of rounds and controls the array depending on round level
+	var count = 0; //counts the number of rounds and controls the array depending on round level
+	var round = 1;
 	var endGame = false; //variable shouold test whether or not the user loses
 	var timeBetweenColors = 1000; //1 second between colors selected by the game.(interval)
 	var lightingColor = 1000; //1 second between seeing a color light up and go off(timeout)
@@ -28,83 +25,51 @@ $(document).ready(function() {
 	var timeOutUser;
 	//-----------------//end variables//--------------------------
 	//function that clears the buttons
-	function clearClicked(color,delay) {
-		//console.log("clearing right away clicked");
-		// $colors.css('opacity', '1');
+	function clearClicked(color, delay) {
 		$(color).animate({
-			opacity : 1
+			opacity: .25
 		}, delay).animate({
-			opacity: 0.25
+			opacity: 1
 		}, delay);
 	}
 	//function that checks the user input
 	function checkClicked() {
-		//it should probably have a timer between divs pressed
-		//function local that waits one second and then gives the turn to the user to input
-		var localTimeOutWait1Second = setTimeout(function() {
-			//change the text so the user knows is his turn
-			$message.text('Repeat the sequence as you just saw.');
-			$round.text('Current round' + count);
-			
-			
-			$divColors.on('click', function(e){
-				console.log("clicked");
-			});
-			$divColors.off('click');
-			//if user misses 
-			//clearInterval(intervalGame);
-			//clearClicked();
-
-			/************////implement later max time out for a round********// 
-			//this function will be the end of the game if the user takes too long to press the colors
-			// timeOutUser = setTimeout(function() {
-			// 		alert('time is UP!');
-			// 		$message.text('GAME OVER!');
-			// 		endGame = true;//end the game condition
-			// 		clearTimeout(timeOutUser);
-			// 	},
-			// 	timeOutvalUser * count); //times count to increase the time depending on the round Ex round 5 = 5 seconds
-		}, 1000);
-
-		//it has to check that the order matches too.
 	}
 	//function that light the sequence that is randomly generated
 	function lightSequence() {
-		for (var i = 0; i < count; i++) {
-			timeOutBetweenDisplayColors = setTimeout(function() {
-				//takes whatever color was randomed in the sequence
-				switch (sequenceOfColors[i]) {
-					/*
-					case 1:red
-					case 2: blue
-					case 3: green
-					case 4: yellow
-					*/
-					//probably need to add a pause between each pressed to give the user time.
-					case 1:
-						//$red.css('opacity', '0.4');
-						clearClicked('#red', 500);
-						break;
-					case 2:
-						// $blue.css('opacity', '0.4');
-						clearClicked('#blue', 500);
-						break;
-					case 3:
-						// $green.css('opacity', '0.4');
-						clearClicked('#green', 500);
-						break;
-					case 4:
-						// $yellow.css('opacity', '0.4');
-						clearClicked('#yellow', 500);
-						break;
-					default:
-						console.log(sequenceOfColors[i]);
-						console.log("dang it default on the swtich");
-						break;
-				}
-			}, lightingColor);
-			// clearClicked();
-		}
+		setTimeout(function(){ 
+			switch (sequenceOfColors[count]) {
+				//probably need to add a pause between each pressed to give the user time.
+				case 1:
+					clearClicked('#red', 300);
+					console.log("red");
+					break;
+				case 2:
+					clearClicked('#blue', 300);
+					console.log("blue");					
+					break;
+				case 3:
+					clearClicked('#green', 300);
+					console.log("green");
+					break;
+				case 4:
+					clearClicked('#yellow', 300);
+					console.log("yellow");
+					break;
+				default:
+					console.log("dang it default on the swtich");
+					break;
+			}
+			count++;
+			if(count<=5){
+				gameStart();
+			}
+			else{
+				console.log("program ends");
+			}
+			
+		}, 1000);
+			
 	}
 	//function that creates the random sequence adding 100 random levels
 	function createSequence() {
@@ -114,26 +79,21 @@ $(document).ready(function() {
 		}
 	}
 	//function that the program uses for creating the random order of color
-	function gameStart() {	
+	function gameStart() {
 		$message.text('Watch the sequence.');
 		$round.text('Current round' + count);
 		lightSequence(); //light the sequence just generated
-
-		//gameContinue();
+		
 	}
 	//function that takes the user input and compares it with the sequence to see if its right. 
 	function gameContinue() {
-		clearClicked(); //function that brings the opacity so square is not clicked
-		//change the text here
-		checkClicked(); //function that checks the user input 
-		//if user misses //this probably has to go inside check clicked
-		// endGame = true; //when the user misses a color in the order
+		
 	}
 	//click for the button
 	$btn.click(function() {
 		$btn.hide();
-		createSequence();//call function to create the sequence just once
-		intervalGame = setInterval(gameStart, timeBetweenColors); //interval that controls the game start and the machine part of the game
-		//timeOutUser = setTimeout(gameContinue, timeUserInput);//time out that will give th
+		createSequence(); //call function to create the sequence just once
+		gameStart();
+
 	});
 });
